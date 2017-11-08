a6739cfefc2d79bb97c2c0e0933a2fb65d4c98af
Mik Kröis Pedersen <mikx0067@edu.easj.dk>
2017-11-08 09:02:39 +0100

    fjernet tokens og koder fra resource

---
+++ TwitchBot/Client.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchLib;
+using TwitchLib.Models.Client;
+using TwitchLib.Events.Client;
+using TwitchLib.Models.API.v5.Users;
+
+namespace TwitchBot
+{
+    class Client
+    {
+        private readonly ConnectionCredentials credentials = new ConnectionCredentials(TwitchRescoure.BotUserName, TwitchRescoure.BotToken);
+        private TwitchClient Twitchclient;
+
+        public void Start()
+        {
+            Twitchclient = new TwitchClient(credentials, TwitchRescoure.ChannelName, logging: false);
+
+
+            Console.WriteLine("Connting");
+            Twitchclient.Connect();
+            Console.WriteLine("connected");
+
+
+
+            Twitchclient.OnMessageReceived += BotClient_OnMessageReceived;
+
+
+
+        }
+
+
+
+        private void BotClient_OnMessageReceived(object sender, OnMessageReceivedArgs e)
+        {
+            string Message = e.ChatMessage.Message.ToLower();
+            switch (Message)
+            {
+                case "!hi":
+                    HelloUserName(sender, e);
+                    break;
+
+                default:
+                    IdidNotUnderStand(e);
+                    break;
+
+            }
+            HelloUserName(sender, e);
+
+        }
+
+
+
+        #region Chat commands
+
+        private void HelloUserName(object sender, OnMessageReceivedArgs e)
+        {
+            Twitchclient.SendMessage($"Hello {e.ChatMessage.DisplayName}");
+        }
+
+        private void IdidNotUnderStand(OnMessageReceivedArgs x)
+        {
+            Twitchclient.Disconnect();
+        }
+
+
+
+        #endregion
+
+
+    }
+}
--- TwitchBot/Program.cs
+++ TwitchBot/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,12 @@ namespace TwitchBot
     {
         static void Main(string[] args)
         {
+            var client = new Client();
+
+            client.Start();
+
+            Console.ReadLine();
         }
+
     }
 }
